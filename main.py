4d7110901f9831186a15126ed3f4e20e393ae7aa
DilZam <dilyar.zamaliev@yandex.ru>
2026-04-23 20:28:38 +0300

    ТЗ + Часть задания выполнено

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='Домашняя страница')
+    return render_template('index.html', title='Главная страница')
+
+
+@app.route('/map')
+def map():
+    return render_template('base.html', title='Карта')
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
