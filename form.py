9c7e62e7d219462089ecf70cefdfa4102f4f40aa
Dilyar <dilyar.zamaliev@yandex.ru>
2026-05-08 21:17:29 +0300

    Код+Пояснительная записка+Презентация+Почти Все работает

--- form.py
+++ form.py
@@ -1,15 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField
 from wtforms.validators import DataRequired, Length, EqualTo
 
 class LoginForm(FlaskForm):
-    email = StringField('Login', validators=[DataRequired()])
+    email = EmailField('Login(email)', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
 
 class RegisterForm(FlaskForm):
-    email = StringField('Login(email)', validators=[DataRequired()])
+    email = EmailField('Login(email)', validators=[DataRequired()])
     name = StringField('Имя', validators=[DataRequired(), Length(min=2, max=50, message="Имя должно быть от 2 до 50 символов")])
     surname = StringField('Фамилия', validators=[DataRequired(), Length(min=2, max=50, message="Фамилия должна быть от 2 до 50 символов")])
     town = StringField('Город', validators=[DataRequired()])
